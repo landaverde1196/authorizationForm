7ddd0622ed90589eb2f180f0c4af5a9098fe19ec
landaverde1196 <alejndro1196@gmail.com>
2020-02-18 20:15:49 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: "swordfish",
+      authorized: false
+    };
+    this.authorize = this.authorize.bind(this);
+  }
+
+  authorize(e) {
+    const password = e.target.querySelector('input[type="password"]').value;
+    const auth = password === this.state.password;
+    this.setState({
+      authorized: auth
+    });
+  }
+
+  render() {
+    const login = (
+      <form action="#" onSubmit={this.authorize}>
+        <input type="password" placeholder="Password" />
+        <input type="submit" />
+      </form>
+    );
+
+    const contactInfo = (
+      <ul>
+        <li>client@example.com</li>
+        <li>555.555.5555</li>
+      </ul>
+    );
+
+    return (
+      <div id="authorization">
+        <h1>{this.state.authorized ? "Contact" : "Enter the Password"}</h1>
+        {this.state.authorized ? contactInfo : login}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Contact;
